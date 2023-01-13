ec996985033746a79d3bb9a1cd74ea1ef8fa3937
trinhan201 <trinhphieuan@gmail.com>
2023-01-13 12:53:24 +0700

    Fix typing error

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,15 +1,12 @@
-import classNames from 'classnames/bind';
-import styles from './DefaultLayout.module.scss';
 import Header from '~/layouts/components/Header';
 import Footer from '~/layouts/components/Footer';
 
-const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div classNames={cx('wrapper')}>
+        <div>
             <Header />
-            <div classNames={cx('container')}>
-                <div classNames={cx('content')}>{children}</div>
+            <div>
+                <div>{children}</div>
             </div>
             <Footer />
         </div>
