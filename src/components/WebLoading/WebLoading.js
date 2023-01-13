50ef1bed348dace47d03aad87a8ba8df536281d5
trinhan201 <trinhphieuan@gmail.com>
2023-01-13 11:38:58 +0700

    Change style of react-spinner

--- src/components/WebLoading/WebLoading.js
+++ src/components/WebLoading/WebLoading.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './WebLoading.module.scss';
-import BarLoader from 'react-spinners/BarLoader';
+import PulseLoader from 'react-spinners/PulseLoader';
 import { webLogo } from '~/assets/img';
 
 const cx = classNames.bind(styles);
@@ -11,7 +11,7 @@ function WebLoading() {
                 <img src={webLogo} alt="" />
             </div>
             <div>
-                <BarLoader color={'#615c85'} loading={true} height={6} width={250} />
+                <PulseLoader color={'#615c85'} loading={true} size={20} />
             </div>
         </div>
     );
